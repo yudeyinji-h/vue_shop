c68c09e0387b1a5058c6009f95c80aa266c57f64
yt <2978486194@qq.com>
2020-03-19 12:32:24 +0800

    complete login

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
+// 弹框提示组件需要挂载到Vue的原型对象上,$message为自定义属性
+Vue.prototype.$message = Message
+
+// 通过Vue.use将这些组件注册为全局可用的组件
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
