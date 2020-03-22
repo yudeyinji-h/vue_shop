92cdfc063c8f5762b551654fced14bb98f4ccbbc
yt <2978486194@qq.com>
2020-03-22 13:39:15 +0800

    fineshed rights

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -23,7 +23,11 @@ import {
   Tooltip,
   Pagination,
   Dialog,
-  MessageBox
+  MessageBox,
+  Tag,
+  Tree,
+  Select,
+  Option
 } from 'element-ui'
 
 // 弹框提示组件需要挂载到Vue的原型对象上,$message为自定义属性
@@ -54,3 +58,7 @@ Vue.use(Switch)
 Vue.use(Tooltip)
 Vue.use(Pagination)
 Vue.use(Dialog)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Select)
+Vue.use(Option)
