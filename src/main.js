1e6d9d67b28be5e26c7afd9a5bfe117ced9421cd
yt <2978486194@qq.com>
2020-03-24 23:04:56 +0800

    completed goodslist

--- src/main.js
+++ src/main.js
@@ -8,6 +8,14 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 // 导入TreeTable第三方插件
 import TreeTable from 'vue-table-with-tree-grid'
+
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
 // 导入axios
 import axios from 'axios'
 // 配置请求的根路径
@@ -23,7 +31,24 @@ Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
+// 注册全局的过滤器
+// dataFormat为过滤器的名称，第二个参数为过滤器的处理函数，originVal为需要被处理的数据
+Vue.filter('dataFormat', function(originVal) {
+  // 根据给定的时间，得到一个时间的日期对象
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
 Vue.component('tree-table', TreeTable)
+
+// 将富文本编辑器注册为全局可用的组件
+Vue.use(VueQuillEditor)
 
 new Vue({
   router,
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -31,7 +31,12 @@ import {
   Cascader,
   Alert,
   Tabs,
-  TabPane
+  TabPane,
+  Steps,
+  Step,
+  CheckboxGroup,
+  Checkbox,
+  Upload
 } from 'element-ui'
 
 // 弹框提示组件需要挂载到Vue的原型对象上,$message为自定义属性
@@ -70,3 +75,8 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(CheckboxGroup)
+Vue.use(Checkbox)
+Vue.use(Upload)
